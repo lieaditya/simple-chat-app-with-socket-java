ff22426166531033082e2a9d7a4bf9662b441583
lieaditya <148756148+lieaditya@users.noreply.github.com>
2024-05-25 00:23:37 +0200

    Set public ip address to localhost

--- src/Client.java
+++ src/Client.java
@@ -14,7 +14,7 @@ public class Client implements Runnable {
     @Override
     public void run() {
         try {
-            client = new Socket( "85.233.56.11", 9999);
+            client = new Socket( "localhost", 50000);
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
